0df25efdeb04f6d11a96aa5770d5651f401745a6
alex <zfmedisonxfmt@126.com>
2016-05-19 20:14:24 +0800

    generate portal controller and routes

--- Gemfile
+++ Gemfile
@@ -34,6 +34,7 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 
 gem 'authlogic', '3.4.6'
 gem "cancan"
+gem 'amazeui'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get 'portal/index'
+
   resources :questions do
-    resources :question_comments,
+    resources :question_comments
     resources :answers do
       resources :answer_comments
     end
@@ -9,7 +11,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+   root 'portal#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'

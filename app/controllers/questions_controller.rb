75d8f7be9f9cfcd391ac9205218919b9a7ed89d7
songzhaozhe <songzhaozhe@126.com>
2016-05-22 12:45:10 +0800

    Implement questions controller

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -22,20 +22,28 @@ class QuestionsController < ApplicationController
   def index
     @questions = Question.all
   end
-  
+
   # Display form to edit current question commenet
   def edit
-
+    @question = Question.find(params[:id])
   end
 
   # Action: Update given question comment
   def update
-
+    @question = Question.find(params[:id])
+    if @question.save
+      redirect_to @root_path
+    else
+      flash[:notice] = "Not successful!"
+      render 'edit'
+    end
   end
 
   # Action: Destroy current question
   # Require question.user == current_user.login || Admin access
   def destroy
-
+    @question = Question.find(params[:id])
+    @question.destroy
+    redirect_to root_path
   end
 end

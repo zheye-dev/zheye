fecfb99ce6bcd5eb881c358466d3d10a69d16066
songzhaozhe <songzhaozhe@126.com>
2016-05-23 19:38:58 +0800

    fix comments_controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -22,6 +22,6 @@ class CommentsController < ApplicationController
   end
   private
   def comment_params
-    params.require(:comment).permit(:text)
+    params.require(:comment).permit(:content)
   end
 end

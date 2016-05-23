b889e1de7de8ab8046057efc48ae24564429fbfa
songzhaozhe <songzhaozhe@126.com>
2016-05-23 19:43:08 +0800

    fix Question_comments_controller

--- app/controllers/question_comments_controller.rb
+++ app/controllers/question_comments_controller.rb
@@ -1,11 +1,11 @@
 class QuestionCommentsController < CommentsController
   # Display all comments to a question
   def index
-    @comments = QuestionComment.where(:answer => params[:answer_id])
+    @comments = QuestionComment.where(answer_id: params[:answer_id])
   end
   # Action: Create a new comment to question
   def create
-    @comment = QuestionComment.create(question: question_id, content: comment_params)
+    @comment = QuestionComment.create(question: question_id, user: current_user, content: comment_params)
   end
 
   # Display an comment box to current question

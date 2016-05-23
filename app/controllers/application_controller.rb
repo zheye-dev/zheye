54b9f5d8d48d9ce1ba1fc9a802f6b066c6efbe68
songzhaozhe <songzhaozhe@126.com>
2016-05-23 19:08:58 +0800

    Fixed compatibility problem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,7 @@ class ApplicationController < ActionController::Base
 
   def current_user
     return @current_user if defined?(@current_user)
-    @current_user = current_user_session&.user
+    @current_user =  @current_user = current_user_session && current_user_session.user
   end
 
   helper_method :current_user_session, :current_user

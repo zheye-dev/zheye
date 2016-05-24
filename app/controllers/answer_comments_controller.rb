3e4079f638c79e783195a74c75668df8869d97ef
songzhaozhe <songzhaozhe@126.com>
2016-05-24 21:16:01 +0800

    rectify answer_comments_controller

--- app/controllers/answer_comments_controller.rb
+++ app/controllers/answer_comments_controller.rb
@@ -1,16 +1,27 @@
 class AnswerCommentsController < CommentsController
+  before_action :get_comment, :only => [:edit]
+  authorize_resource
+  def get_comment
+    @comment = AnswerComment.find(params[:answer_comment_id])
+  end
   # Display all comments to an answer
   def index
     @comments = AnswerComment.where(answer_id: params[:answer_id])
   end
   # Action: Create a new comment to answer
   def create
-    @comment_parent = Answer.find(params[:answer_id])
-    @comment = AnswerComment.new(question_id: @comment_parent.question_id, answer: @comment_parent, user: current_user, content: answer_comment_params[:content])
+    #@question = Question.find(params[:question_id])
+    @answer = Answer.find(params[:answer_id])
+    @comment = AnswerComment.new(answer_comment_params)
+    @comment.answer = @answer
+    #@comment.question = @question
+    @comment.user = current_user
     if @comment.save
       redirect_to @comment.answer.question
+      flash[:notice] = 'Comment created!'
     else
       render 'comment/new'
+      flash[:notice] = 'Failed!'
     end
   end
 
@@ -18,14 +29,12 @@ class AnswerCommentsController < CommentsController
   def new
     @comment = AnswerComment.new
     @comment.answer = Answer.find(params[:answer_id])
-    #render 'comments/new'
   end
 
   # Display form to edit current answer comment
   def edit
-    @comment = AnswerComment.find(params[:answer_comment_id])
-    authorize! :update, @comment
   end
+
   private
   def answer_comment_params
     params.require(:answer_comment).permit(:content)

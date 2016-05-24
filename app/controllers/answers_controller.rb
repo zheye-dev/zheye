31f915f422cf0fc2a87b8e89a3e61bc0130d91a4
songzhaozhe <songzhaozhe@126.com>
2016-05-24 19:55:29 +0800

    test

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -40,7 +40,7 @@ class AnswersController < ApplicationController
   # Action: Update given answer
   def update
     @question = Question.find(params[:question_id])
-    @answer = Answer.find(params[:answer_id])
+    @answer = Answer.find(params[:id])
     @answer.update(answer_params)
     #authorize! :update, @answer
     redirect_to @question

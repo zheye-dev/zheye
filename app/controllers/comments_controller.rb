e65124c4f3cac990ca5ea3891b6e8330c52e2893
songzhaozhe <songzhaozhe@126.com>
2016-05-24 21:02:27 +0800

    comments_controller before_action :get_comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,18 +1,22 @@
 # Virtual class for comments, don't call directly
 class CommentsController < ApplicationController
+  before_action :get_comment, :only => [:update,:destroy]
   authorize_resource
-
+  def get_comment
+    @comment = Comment.find(params[:id])
+  end
   # Action: Update given comment
   def update
-    @comment = Comment.find(params[:id])
-    @comment.update(comment_params)
-    flash[:notice] = 'Comment edited!'
+    if @comment.update(comment_params)
+      flash[:notice] = 'Comment edited!'
+    else
+      render 'comment/new'
+    end
   end
 
   # Action: Destroy current comment
   # Require comment.user == current_user.login || Admin access
   def destroy
-    @comment = Comment.find(params[:id])
     @comment.destroy
     redirect_to root_path
   end

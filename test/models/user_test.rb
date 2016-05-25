07671c86c9dbe4ddbca06c7b3959c826c786ad49
qu10wenhao <shark_010@163.com>
2016-05-24 20:40:50 -0700

    test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
   test "destroy questionswhile user destroyed" do
-    user = User.create(login: "tester", password: "password", password_confirmation: "password")
+    user = User.create(login: "tester1", password: "password", password_confirmation: "password")
     question = user.questions.create(user: user, title:"test title", content: "this is a test question")
     assert_equal question.user,user
     assert user.persisted?
@@ -13,21 +13,25 @@ class UserTest < ActiveSupport::TestCase
   end
 
   test "destroy answers while user destroyed" do
-    user = User.new(login: "tester", password: "password", password_confirmation: "password")
+    user = User.new(login: "tester2", password: "password", password_confirmation: "password")
     user.save
-    question = Question.create(user: user, title:"test title", content: "this is a test question")
+    question = user.questions.create(user: user, title:"test title", content: "this is a test question")
     answer = Answer.create(question_id: question.id, user: user, content: "this is a test answer")
     assert_equal answer.user,user
-
+    answerid = answer.id
     assert user.persisted?
     assert answer.persisted?
+    answer_p = Answer.find(answerid)
+    assert_equal answer_p, answer
     user.destroy
-    assert_not user.persisted?, "destroy user unsuccess"
-    assert_not answer.persisted?,"destroy answer unsuccess"
+    answer_p = Answer.find(answerid)
+    assert user.destroyed?, "destroy user unsuccess"
+    assert question.destroyed?,"destroy question unsuccess"
+    assert answer.destroyed?,"destroy answer unsuccess"
   end
 
   test "destroy comments while user destroyed" do
-    user = User.new(login: "tester", password: "password", password_confirmation: "password")
+    user = User.new(login: "tester3", password: "password", password_confirmation: "password")
     user.save
     question = Question.create(user_id: user.id, title:"test title", content: "this is a test question")
     comment = QuestionComment.create(question_id: question.id, user_id: user.id, content: "this is a test commont")

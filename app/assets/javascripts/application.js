3a93cb72cf4ff1b87b0829cb6f6d901b5f456291
alex <zfmedisonxfmt@126.com>
2016-05-27 21:46:51 +0800

    modify footer and index sort button

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,9 +13,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
-//= require amazeui.js
 //= require amazeui.min.js
-//= require amazeui.widgets.helper.js
 //= require amazeui.widgets.helper.min.js
 //= require handlebars.min.js
 //= require ckeditor/init

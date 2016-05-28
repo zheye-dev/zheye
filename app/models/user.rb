91b12ddbd5b5c105b6050fb28b3d38fef7f27e0e
Vic Luo <vicluo96@gmail.com>
2016-05-28 14:20:19 +0800

    Implement all_votes_received

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,15 @@ class User < ActiveRecord::Base
     has_many :questions, dependent: :destroy
     has_many :answers, dependent: :destroy
     has_many :comments, dependent: :destroy
+
+    def all_upvotes_received
+        upvotes_count =
+            self.questions.inject(0) do |count, q|
+                count += q.question_votes.where(attitude: 1).length
+            end
+        self.answers.inject(upvotes_count) do |count, q|
+            count += q.answer_votes.where(attitude: 1).length
+        end
+
+    end
 end

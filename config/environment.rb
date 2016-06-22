27b6493410e80d6f211b48f9fd576b544af837a9
qu10wenhao <shark_010@163.com>
2016-06-22 06:44:41 -0700

    json test finished problem with comments and votes

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,5 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+Authlogic::Session::Base.controller = Authlogic::ControllerAdapters::RailsAdapter.new(self)
